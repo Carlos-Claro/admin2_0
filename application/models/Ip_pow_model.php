ca3207602f1722c57eaf960dc2c5cdaeb26e7e16
Carlos Claro em POW <programacao@pow.com.br>
2016-07-15 18:33:34 -0300

    ip_pow

---
+++ application/models/Ip_pow_model.php
@@ -0,0 +1,38 @@
+<?php
+class Ip_pow_Model extends MY_Model {
+	
+    private $database = NULL;
+    
+    public function __construct()
+    {
+        // Call the Model constructor
+        parent::__construct();		
+        $this->database = array('db' => 'guiasjp', 'table' => 'ip_pow');
+    }
+	
+    public function adicionar( $data = array() )
+    {
+        return $this->adicionar_($this->database, $data);
+    }
+    
+    public function editar($data = array(),$filtro = array())
+    {
+        return $this->editar_($this->database, $data, $filtro);
+    }
+    
+    public function excluir($filtro)
+    {
+        return $this->excluir_($this->database, $filtro);
+    }
+    
+    public function get_item( $id = '' )
+    {
+    	$data['coluna'] = '*';
+    	$data['tabela'] = array(
+                                array('nome' => 'ip_pow'),
+                                );
+    							
+    	$retorno = $this->get_itens_($data);
+    	return $retorno['itens'][0];
+    }
+}
